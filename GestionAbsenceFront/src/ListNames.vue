77545a79d9b44287f77fc8668cc8ccae1a5158e2
lenakieffel <kieffellena@gmail.com>
2024-12-02 19:03:50 +0100

    Json File

---
+++ GestionAbsenceFront/src/ListNames.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+import { jsonData } from './ListNamesStu';
+
+const multiSelected = ref([])
+</script>
+<template>
+  <h2>Liste de Noms</h2>
+  <select v-model="multiSelected" multiple style="width:500px">
+    <option v-for="(surname) in jsonData.surnames" :value="surname">
+        {{ surname }}
+    </option>
+
+  </select>
+  <p>Selected: {{ multiSelected }}</p>
+</template>
