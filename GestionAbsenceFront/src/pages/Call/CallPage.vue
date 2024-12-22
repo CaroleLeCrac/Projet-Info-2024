50981803e235c5c10f3f09c9634c88fa1bbcf186
roxdbs <roxane.dubus4@gmail.com>
2024-12-22 13:39:05 +0100

    ajout du bouton sauvegarde dans CallPage

--- GestionAbsenceFront/src/pages/Call/CallPage.vue
+++ GestionAbsenceFront/src/pages/Call/CallPage.vue
@@ -3,13 +3,35 @@
     <div>
         <h1>Appel</h1>
         <SelectionPresenceList/>
-        <button class="button-save">Sauvegarder</button>
+        <!--Le bouton doit apparaitre à chaque fois qu'un changement est fait dans la liste-->
+        <!--dans le composant, afficher présent.e et absent.e dans la liste à côté du nom ?-->
+        <!--dans le composant, ajouter un bouton voir le récap de l'étudiant lorsqu'on le survole-->
+        <button 
+            v-if="!callSaved" 
+            class="button-save" 
+            @click="saveCall"
+        >Sauvegarder l'appel</button>
+        <p v-if="showNotification" class="notification">Appel sauvegardé avec succès</p>
     </div>
 </template>
 
 <script setup>
+
+import { ref } from 'vue';
 import SelectionPresenceList from './SelectionPresenceList.vue';
 
+const callSaved = ref(false);
+const showNotification = ref(false);
+
+function saveCall() {
+    /* Sauvegarder l'appel */
+    callSaved.value = true;
+    showNotification.value = true;
+
+    setTimeout(() => {
+        showNotification.value = false;
+    }, 3000); // temps d'affichage du message de sauvegarde de l'appel
+}
 
 </script>
 
@@ -21,6 +43,10 @@ import SelectionPresenceList from './SelectionPresenceList.vue';
     border-radius: 5px;
     cursor: pointer;
     font-size: 16px;
+}
+
+.notification {
+    color: rgb(94, 216, 94);
 }
 
 .button-save:hover {
