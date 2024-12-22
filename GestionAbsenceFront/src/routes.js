95cc0d7e691b20598019c67642fb30f7eab76ba0
CaroleLeCrac <carolemitton01@gmail.com>
2024-12-22 18:47:10 +0100

    correction import

--- GestionAbsenceFront/src/routes.js
+++ GestionAbsenceFront/src/routes.js
@@ -1,7 +1,6 @@
 import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from './pages/HomeView/HomeView.vue'
-import ListNames from './shared/components/ListNames.vue'
 import SelectStudentModification from './pages/SelectStudentModification/SelectStudentModification.vue'
 import GroupModification from './pages/GroupModification/GroupModification.vue'
 import StudentModification from './pages/StudentModification/StudentModification.vue'
@@ -9,17 +8,15 @@ import SelectGroupModification from './pages/SelectGroupModification/SelectGroup
 import SlotPage from './pages/Slot/SlotPage.vue'
 import CallPage from './pages/Call/CallPage.vue'
 import SummaryPage from './pages/Summary/SummaryPage.vue'
-import SelectGroupModification from './pages/SelectGroupModification/SelectGroupModification.vue'
+
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/creneau', component: SlotPage },
   { path: '/appel', component: CallPage },
   { path: '/récapitulatifs', component: SummaryPage },
-  { path: '/select/modification/student', component: SelectStudentModification },
-  { path: '/select/modification/group', component: SelectGroupModification},
-  { path: '/modification/group', component : GroupModification},
-  { path: '/modification/student', component : StudentModification},
+  { path: '/modificationEtudiant', component: SelectStudentModification },
+  { path: '/modificationGroupe', component: SelectGroupModification},
   { path: '/modification/groupe', component : GroupModification},
   { path: '/modification/etudiant', component : StudentModification}
 ]
