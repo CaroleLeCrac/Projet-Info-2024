af725f73fa06be3934aeb8eb4f673610fdadceb7
CaroleLeCrac <carolemitton01@gmail.com>
2025-01-14 18:54:03 +0100

    Changements architecture

--- GestionAbsenceFront/src/pages/SelectGroupModification/SelectGroupModification.vue
+++ GestionAbsenceFront/src/pages/SelectGroupModification/SelectGroupModification.vue
@@ -1,11 +1,61 @@
 <!--Page de sélection d'un groupe pour ajouter ou supprimer des étudiants-->
-<template>
-
-    <h2>Choisissez un groupe à modifier</h2>
-<ListGroup/>
-</template>
-
 <script setup>
-import ListGroup from './ListGroup.vue';
+import { ref, onMounted } from 'vue'
+
+const groups = ref([])
+
+
+  onMounted(() => {
+    fetch('/group.json')
+    .then((response)=>response.json())
+    .then ((data)=> {
+      console.log("Données JSON récupérées : ", data);
+      groups.value = data.groups;
+    })
+    .catch(error => console.error('Error loading data:', error));
+});
 
 </script>
+<template>
+  <h2>Choisissez un groupe à modifier</h2>
+  <ul class="group-list">
+    <li v-for="group in groups" :key="group.groupNumber">
+      <label class="container">
+        <RouterLink :to="`/modification/groupe/${group.groupNumber}`" class="router-link">{{ group.name }}</RouterLink>
+      </label>
+      </li>
+  </ul>
+</template>
+
+<style scoped>
+
+
+.router-link{
+  display: block;
+  text-decoration: none; 
+  border-radius: 5px;
+  color: black; 
+  cursor: pointer; 
+  font-size: 16px; 
+  padding: 8px 10px; 
+  width: 50%;
+  border: 2px solid black;
+  box-sizing: border-box;
+}
+
+.container{
+  display : block;
+    position :relative; 
+    margin-bottom: -1px;
+    font-size: 16px;
+    user-select: none;
+}
+
+.group-list{
+  display : flex; 
+  flex-direction: column;
+  width: 40%;
+  list-style-type: none;
+  padding-left: 0;
+}
+</style>
